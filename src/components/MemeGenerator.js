ea273dd780f351f40d73099875b8ba9f7af0f5a8
Rhys Malyon <malyonr@gmail.com>
2021-10-10 18:01:17 +0900

    Add error handling

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -7,23 +7,33 @@ class MemeGenerator extends Component {
       topText: "",
       bottomText: "",
       randomImage: "",
-      allMemeImages: []
+      allMemeImages: [],
+      error: ""
     }
   }
 
   componentDidMount() {
     fetch("https://api.imgflip.com/get_memes")
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw Error('Could not fetch the data')
+        }
+        return response.json()
+      })
       .then(response => {
         const { memes } = response.data
         this.setState({ allMemeImages: memes })
+      })
+      .catch(err => {
+        const errorMessage = err.toString()
+        this.setState({ error: errorMessage })
       })
   }
 
   render() {
     return (
       <div>
-        <p>Test</p>
+        <p>{this.state.error}</p>
       </div>
     )
   }
